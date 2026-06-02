4ebba6165639b963cfdefe5e2134258260bcac39
bablx <babix0068@gmail.com>
2026-06-03 01:15:06 +0530

    Fix icon.js to use proper NextResponse for Next.js App Router

--- client/src/app/icon.js
+++ client/src/app/icon.js
@@ -1,12 +1,16 @@
-import Image from 'next/image';
+import { NextResponse } from 'next/server';
+import mia from '../../../public/mia.png';
 
-export default function Icon() {
-  return (
-    <Image
-      src="/mia.png"
-      alt="MIARUSH"
-      width={32}
-      height={32}
-    />
-  );
+export const runtime = 'edge';
+
+export async function GET() {
+  const response = await fetch(mia.src);
+  const imageBuffer = await response.arrayBuffer();
+  
+  return new NextResponse(imageBuffer, {
+    headers: {
+      'Content-Type': 'image/png',
+      'Content-Disposition': 'inline; filename="icon.png"',
+    },
+  });
 }
